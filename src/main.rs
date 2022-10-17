61cce6498592fc4620d941deeb51553b08abf22c
Paul Thomas <paulthomas2107@gmail.com>
2022-10-17 20:49:02 +0100

    More...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#![allow(unused)]
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Paul 01");
 }
